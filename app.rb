d9783345d9f91a2d9a5689012c433cde72fe9e60
Jerry Jimenez <jerry.jimenez@irstrat.com>
2017-06-01 13:15:25 -0500

    Agregar la gema better errors

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require "rubygems"
 require "sinatra"
+require 'better_errors' if development?
+
+
+configure :development do
+ use BetterErrors::Middleware
+ BetterErrors.application_root = __dir__
+end
 
 get "/" do
-  @name = 'IRStrat'
+  @name = 2/0
   "Hola" + @name
 end
 
